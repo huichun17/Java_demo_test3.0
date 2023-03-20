8a57a8c74453174ec640f6db1762944f8d8c857e
huichun17 <monday20230220@gmail.com>
2023-03-20 11:46:47 +0800

    no message

---
+++ src/test/java/com/example/Java_demo_test30/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_demo_test30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
